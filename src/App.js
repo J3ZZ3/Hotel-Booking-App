7274cadcaf5626e14ed173b6307b55de8661b729
Jesse <jesse.mashoana@gmail.com>
2025-01-23 10:28:41 +0200

    env

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import AdminDashboard from "./components/admin/AdminDashboard";
 import AdminLogin from "./components/admin/AdminLogin";
 import AddAdmin from "./components/admin/AddAdmin";
 import ProtectedRoute from "./components/ProtectedRoute"; // Import ProtectedRoute
+import ClientDashboard from "./components/client/ClientDashboard"; // Import ClientDashboard
+import ClientLogin from "./components/client/ClientLogin"; // Import ClientLogin
+import BookingHistory from "./components/client/BookingHistory"; // Import BookingHistory
+import BookingStatus from "./components/client/BookingStatus"; // Import BookingStatus
+import ClientRegister from "./components/client/ClientRegister"; // Import ClientRegister
 
 const App = () => {
   return (
@@ -25,6 +30,33 @@ const App = () => {
             element={
               <ProtectedRoute>
                 <AddAdmin />
+              </ProtectedRoute>
+            }
+          />
+          {/* Client Routes */}
+          <Route path="/client-login" element={<ClientLogin />} />
+          <Route path="/client-register" element={<ClientRegister />} />
+          <Route
+            path="/client-dashboard"
+            element={
+              <ProtectedRoute>
+                <ClientDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/booking-history"
+            element={
+              <ProtectedRoute>
+                <BookingHistory />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/booking-status/:roomId"
+            element={
+              <ProtectedRoute>
+                <BookingStatus />
               </ProtectedRoute>
             }
           />
